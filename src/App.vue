03c066bc9eae5cb3d58b3298f8d3ba8b1a980177
Kristen48 <kristen.kedus@gmail.com>
2020-11-23 18:11:33 +0200

    Login page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <router-view></router-view>
-  </div>
+  </div> -->
+    <section class="login-page">
+        <!-- <router-view></router-view> -->
+      <section id="login-container">
+        <div>
+          <h1>Welcome to postIt!</h1>
+        </div>
+        <div>
+          <h4>
+            <a href="#">Create an Account</a>
+          </h4>
+        </div>
+        <div>
+          <h4>Or</h4>
+        </div>
+        <div>
+          <h3>Please Log In</h3>
+          <form action="index.html" method="post">
+            <div>
+              <input type="text" name="email" placeholder="Email">
+            </div>
+            <div>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+            <div>
+                <button class="button" type="submit" name="login">Log In</button>
+            </div>
+            <div>
+              <small>
+                <a href="#">Forgot password?</a>
+              </small>
+            </div>
+          </form>
+        </div>
+      </section>
+    </section>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
+    name: 'App',
+    computed: {
 
-  }
+    }
 }
 </script>
 
@@ -35,12 +69,30 @@ body {
 a {
     color: #40c4ff;
 }
-.main-container {
-    width: 50%;
-    min-height: 100%;
-    margin: auto auto;
-    padding: 90px 15px 15px 15px;
+
+.login-page {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#login-container {
+    /* background-image: url(../images/logo.png);
+    background-size: 20% auto;
+    background-repeat: no-repeat; */
+    background-position: 15px 15px;
+    min-width: 25%;
+    padding: 45px 45px;
     background-color: #ffffff;
+    box-shadow: 0 0 15px rgba(38, 50, 56, 0.33);
+    text-align: center;
+}
+
+#login-container form div > input {
+    padding: 8px 16px;
+    margin: 4px 0;
 }
 
 button {
@@ -56,4 +108,13 @@ button:hover {
     box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
     cursor: pointer;
 }
+
+.main-container {
+    width: 50%;
+    min-height: 100%;
+    margin: auto auto;
+    padding: 90px 15px 15px 15px;
+    background-color: #ffffff;
+}
+
 </style>
