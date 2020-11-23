499c12172e409c31ab3effecab253499788fc045
Anželika Bidzilja <bidzilja@ut.ee>
2020-11-23 18:02:30 +0200

    Main page changes

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,58 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-
-export default {
-  name: 'app',
-  components: {
-
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+        }
+    }
 </script>
 
 <style>
-* {
-    font-family: 'Roboto Slab', serif;
-    outline: none;
-}
+    * {
+        font-family: 'Roboto Slab', serif;
+        outline: none;
+    }
 
-html, body {
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 100%;
-}
+    html, body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
 
-body {
-    background-color: #0277bd;
-    color: #263238;
-}
+    body {
+        background-color: #0277bd;
+        color: #263238;
+    }
 
-a {
-    color: #40c4ff;
-}
-.main-container {
-    width: 50%;
-    min-height: 100%;
-    margin: auto auto;
-    padding: 90px 15px 15px 15px;
-    background-color: #ffffff;
-}
+    a {
+        color: #40c4ff;
+    }
 
-button {
-    padding: 8px 16px;
-    margin: 4px 0;
-    color: #ffffff;
-    background-color: #01579b;
-    border: none;
-    border-radius: 4px;
-}
+    .main-container {
+        width: 50%;
+        min-height: 100%;
+        margin: auto auto;
+        padding: 90px 15px 15px 15px;
+        background-color: #ffffff;
+    }
 
-button:hover {
-    box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
-    cursor: pointer;
-}
+    button {
+        padding: 8px 16px;
+        margin: 4px 0;
+        color: #ffffff;
+        background-color: #01579b;
+        border: none;
+        border-radius: 4px;
+    }
+
+        button:hover {
+            box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
+            cursor: pointer;
+        }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import Browse from './components/Browse.vue'
-import Profile from "./models/Profile";
+import Profile from "./models/Profile"
+import Post from "./models/Post"
+import Main from './components/Main.vue'
 
 
 Vue.config.productionTip = false
@@ -12,7 +14,8 @@ Vue.use(Vuex);
 
 const routes = [
     {path: '/browse', component: Browse},
-    {path: '/profiles/:id', name: "profiles", component: Browse}
+    { path: '/profiles/:id', name: "profiles", component: Browse },
+    { path: '/main', component: Main },
 ];
 
 const router = new VueRouter({routes});
@@ -25,6 +28,12 @@ const store = new Vuex.Store({
             new Profile("John", "Doe", "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"),
             new Profile("Bruce",  "Wayne", "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"),
             new Profile("Richard", "Stallman","https://images.unsplash.com/photo-1553798194-cc0213ae7f99?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80")
+        ],
+        posts: [
+            new Post("http://www.pastatdude.com/uploaded_images/hl2-2007-10-20-16-36-36-32-713089.jpg", "I think it's going to rain"),
+            new Post(null, "Which weighs more, a pound of feathers or a pound of bricks?"),
+            new Post("https://i.pcmag.com/imagery/reviews/00EfzjLJNL6FNKVxviGg7Zw-2.1569473216.fit_scale.size_1182x667.jpg", "Felt cute, might delete later"),
+            new Post("https://assets.mixkit.co/videos/preview/mixkit-software-developer-working-on-code-screen-close-up-1728-large.mp4", null)
         ]
     },
     mutations: {
